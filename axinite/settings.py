c5568423280b92f36be085ef6b80760664048e46
Nikhil Verma <varma.nikhil22@gmail.com>
2012-10-14 19:02:37 +0530

    Correcting static urls

--- axinite/settings.py
+++ axinite/settings.py
@@ -72,7 +72,6 @@ STATIC_ROOT = "axinite/static"
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/static/'
-print STATIC_URL
 # URL prefix for admin static files -- CSS, JavaScript and images.
 # Make sure to use a trailing slash.
 # Examples: "http://foo.com/static/admin/", "/static/admin/".
